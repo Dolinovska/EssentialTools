510c59851a7798d75fb3d7db72f5676a758c07c1
sdolinovska <sdolinovska@gmail.com>
2019-08-31 19:58:54 +0300

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System.Web.Mvc;
+using EssentialTools.Models;
+
+namespace EssentialTools.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IValueCalculator _calc;
+        private Product[] products =
+        {
+            new Product {Name = "Kayak", Category = "Watersports", Price = 275M},
+            new Product {Name = "LifeJacket", Category = "Watersports", Price = 48.95M},
+            new Product {Name = "Soccer ball", Category = "Soccer", Price = 19.50M},
+            new Product {Name = "Corner flag", Category = "Soccer", Price = 34.95M},
+        };
+
+        public HomeController(IValueCalculator calcParam, IValueCalculator calc2)
+        {
+            _calc = calcParam;
+        }
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            ShoppingCart cart = new ShoppingCart(_calc) { Products = products };
+            decimal totalValue = cart.CalculateProductTotal();
+
+            return View(totalValue);
+        }
+    }
+}
